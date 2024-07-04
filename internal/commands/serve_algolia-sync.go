80d4b8b7d5f84e006ef253bd6aa5dbe9eafb5d03
James <admin@floretos.com>
2024-07-04 11:58:11 -0400

    fix: command

--- internal/commands/serve_algolia-sync.go
+++ internal/commands/serve_algolia-sync.go
@@ -11,7 +11,7 @@ import (
 
 // serveCmd represents the serve command
 var serveAlgoliaSyncCmd = &cobra.Command{
-	Use:   "serve-image-sync",
+	Use:   "serve-algolia-sync",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
